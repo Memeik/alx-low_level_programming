8fa7ccfb61488f8dc720fb8f586b5907364406b3
Memeik <okorieonyedikachi9@gmail.com>
2022-09-08 07:56:57 -0700

    4-5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main()
+/**
+ * print a string
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main() 
+
+/**
+ * code to print a string
+ */
+int main(void) 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
