9d582288df9d33294a05eef43ce37f38210c71a4
Memeik <okorieonyedikachi9@gmail.com>
2022-09-08 07:36:32 -0700

    all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main()
 	printf("Size of a char: %d byte(s)\n", sizeof(f));
 	printf("Size of an int: %d byte(s)\n", sizeof(a));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %d byte\n", sizeof(d));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 	printf("Size of a float: %d byte(s)\n", sizeof(b));
 	return (0);
 }
