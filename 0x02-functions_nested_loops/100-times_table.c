4d70a96cbb30c620cea95a78582f454065280f40
Memeik <okorieonyedikachi9@gmail.com>
2022-09-13 14:04:24 -0700

    t

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints a multiplication table up to param
+ *
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
+ */
+
+void print_times_table(int n)
+{
+	int x, y, z, u, d;
+
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z>99){
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 100) + '0')
+					_putchar(((z / 10) % 10) + '0')
+					_putchar((z % 10) + '0')
+			}
+			else if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
