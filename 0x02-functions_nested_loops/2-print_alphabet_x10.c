afe11501f113af7a36770d3370e952f9fb60d4a1
Memeik <okorieonyedikachi9@gmail.com>
2022-09-13 06:30:22 -0700

    f2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print *10
+ *
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ *
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i=0;	
+	char x;
+
+	while(i<10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
