a75008902a79ca001af11b8beb44508d36807672
Memeik <okorieonyedikachi9@gmail.com>
2022-09-14 13:31:29 -0700

    l

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints the fib
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	long int a, b, n, result;
+
+	a = 1;
+	b = 2;
+	n = 0;
+	while(n <= 98)
+	{
+		if(n == 98)
+		{
+			printf("%ld\n", a);
+		}
+		else
+		{
+			printf("%ld, ", a);
+		}
+		result = a + b;
+		a = b;
+		b = result;
+		n++;
+	}
+
+	return (0);
+}
