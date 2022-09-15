f9689739c15ac7b9e18362ff14d491fe6b26910b
Memeik <okorieonyedikachi9@gmail.com>
2022-09-15 06:07:56 -0700

    f4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' && c == '4')
+		{
+			continue;
+		}
+		_putchar(c);
+	}
+	_putchar('\n');
+}
