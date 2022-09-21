15d1f4f1c203f40614dc32be2a648f390f1ca9cc
Memeik <okorieonyedikachi9@gmail.com>
2022-09-21 12:18:26 -0700

    first

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,22 @@
 #include "main.h"
-#include "stdio.h"
 
 /**
  * _strcmp - a function that compares two strings.
+ *
  * @s1: an input string
+ *
  * @s2: an input string
- * Return: The sum of s1 + s2
+ *
+ * Return: The difference s1 - s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0;
-
 	while (*s1 && *s2)
 	{
-		if (s1[a] > s2[a])
-		{
-			return (15);
-		}
-		else if (s1[a] < s2[a])
-		{
-			return (-15);
-		}
-		else
-		{
-			return (a);
-		}
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
 	}
+	return (*s1 - *s2);
 }
