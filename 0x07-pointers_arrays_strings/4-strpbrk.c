c6d24c824a7b74b29ffce7e24620d1985c974874
Memeik <okorieonyedikachi9@gmail.com>
2022-09-26 13:05:48 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,20 +5,19 @@
  * @s: pointer where we search for a char
  * @accept: char been searched for
  * Return: Null
- */
+ **/
 
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i]!= '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-				return (s[i]);
+			if (*(s + i) == *(accept + j))
+				return (s + i);
 		}
 	}
-
 	return ('\0');
 }
