1ebcb4809fb3b59dd795cb4f7bb98f2797ab9406
Memeik <okorieonyedikachi9@gmail.com>
2022-09-26 12:23:34 -0700

    1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: assigned a pointer char var
+ * @c: assigned char var
+ * Return: c
+ */
+
+char *_strchr(char *s, char c);
+{
+	unsigned int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c) 
+		{
+			return (s);
+			break;
+		}
+		i++
+	}
+	return 0;
+}
