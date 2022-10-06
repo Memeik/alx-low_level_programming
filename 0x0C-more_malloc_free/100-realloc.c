a6e2129c2ca304872d6b256dd9fd1ce19d2036fa
Memeik <okorieonyedikachi9@gmail.com>
2022-10-06 06:49:43 -0700

    sigh

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_location;
+	char *new_location, *new_ptr;
 	unsigned int i = 0;
 
 	if (new_size == old_size)
@@ -30,10 +30,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	new_location = malloc(new_size);
+	new_ptr = ptr;
 	if (old_size > new_size)
 		old_size = new_size;
 	for (i = 0; i < old_size; i++)
-		new_location[i] = ptr[i];
+		new_location[i] = new_ptr[i];
 	free(ptr);
 	return (new_location);
 }
