13f4cc585eb46b390bf5df8148c87f67a8184bd4
Memeik <okorieonyedikachi9@gmail.com>
2022-10-06 06:46:08 -0700

    a4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,31 +16,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 
 		if (ptr == NULL)
 			return (NULL);
-
 		return (ptr);
 	}
-
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	new_location = malloc(new_size);
-
 	if (old_size > new_size)
 		old_size = new_size;
-
 	for (i = 0; i < old_size; i++)
 		new_location[i] = ptr[i];
-
 	free(ptr);
 	return (new_location);
 }
